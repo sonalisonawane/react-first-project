a26ea8ec68a8ad7f00942a17033ee973319004cd
sonalisonawane <sonawanesonali431@gmail.com>
2025-01-06 22:27:38 +0530

    code added in app.js for dynamic bg color change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [input, setInput] = useState("");
+  const changeHandler = (e)=> {
+    const inputValue = e.target.value;
+    if (inputValue.charAt(0) === "#" ){
+     console.log(inputValue.length)
+      if(inputValue.length===4 || inputValue.length===7){
+        document.body.style.backgroundColor = inputValue;
+      }
+      else{
+        document.body.style.backgroundColor = "#FFF";
+      }
+    }
+    setInput(inputValue);
+  }
   return (
-    <div className="App">
-      <h1>React First Website</h1>
-    </div>
+    <>
+    <h1>Hi React Learner</h1>
+    <input onChange={changeHandler} value={input}/>
+    <p>{input}</p>
+    </>
   );
 }
 
-export default App;
+export default App
